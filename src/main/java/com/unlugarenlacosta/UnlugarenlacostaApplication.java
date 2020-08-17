07bdc05a39f6e38e5d1e95979824ae8478158062
HeraWer <vallejestudiante@gmail.com>
2020-08-17 18:35:43 +0200

    first commit

---
+++ src/main/java/com/unlugarenlacosta/UnlugarenlacostaApplication.java
@@ -0,0 +1,13 @@
+package com.unlugarenlacosta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnlugarenlacostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnlugarenlacostaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unlugarenlacosta/UnlugarenlacostaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unlugarenlacosta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnlugarenlacostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
